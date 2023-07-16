8702af8c49b207c327280a3d3d2a7beed5cc40a8
sadatanwar18 <125532990+sadatanwar18@users.noreply.github.com>
2023-07-16 23:24:03 +0530

    Add files via upload

---
+++ Practice/ques_3.java
@@ -0,0 +1,26 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class ques_3 {
+
+    // nth term of fibonacci series
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int a = 0;
+        int b = 1;
+        int count = 2;
+
+        while (count <= n){
+            int temp = b;
+            b = b + a;
+            a = temp;
+            count++;
+        }
+
+        System.out.println(b);
+    }
+}
