f2bbbead36bc092037f2ab7524fe565dd22c1510
sadatanwar18 <125532990+sadatanwar18@users.noreply.github.com>
2023-08-02 03:32:04 +0530

    Add files via upload

---
+++ Array_question_tuf/Ques1.java
@@ -0,0 +1,21 @@
+package Array_question_tuf;
+
+public class Ques1 {
+    public static void main(String[] args) {
+
+        
+        int arr1[] =  {2,5,1,3,0};
+
+        System.out.println(largestelement(arr1));
+    }
+
+    static int largestelement (int arr[]){
+            int max = arr[0];
+            for (int i = 0; i < arr.length ; i++){
+                if (arr[i] > max){
+                    max = arr[i];
+                }
+            }
+            return max;
+    }
+}
